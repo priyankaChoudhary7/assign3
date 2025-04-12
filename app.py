eb41505076cde12d87c3587fa947f22cccb8d483
Priyanka Choudhary <52857047+priyankaChoudhary7@users.noreply.github.com>
2025-04-12 12:57:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from Choudhary ECS Container.”</h1>"
+    return "<h1>Hello from Choudhary ECS Container.</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
